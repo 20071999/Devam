48797ebb3bfc6ca59e072c21e052165694133c9f
20071999 <46691075+20071999@users.noreply.github.com>
2019-01-14 23:44:45 +0300

    Add files via upload

---
+++ Devam/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Devam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kaldırToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.hakkındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programınAmacıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.manuelEkleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(973, 224);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Program Adı";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Program Yolu";
+            this.columnHeader2.Width = 627;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Executable Adı";
+            this.columnHeader3.Width = 179;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(973, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView2.Location = new System.Drawing.Point(992, 198);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(77, 128);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Program Adı";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Program Yolu";
+            this.columnHeader5.Width = 94;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Executable Adı";
+            this.columnHeader6.Width = 111;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kaldırToolStripMenuItem,
+            this.hakkındaToolStripMenuItem,
+            this.programınAmacıToolStripMenuItem,
+            this.manuelEkleToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(168, 114);
+            // 
+            // kaldırToolStripMenuItem
+            // 
+            this.kaldırToolStripMenuItem.Name = "kaldırToolStripMenuItem";
+            this.kaldırToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.kaldırToolStripMenuItem.Text = "Kaldır";
+            this.kaldırToolStripMenuItem.Click += new System.EventHandler(this.kaldırToolStripMenuItem_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // hakkındaToolStripMenuItem
+            // 
+            this.hakkındaToolStripMenuItem.Name = "hakkındaToolStripMenuItem";
+            this.hakkındaToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.hakkındaToolStripMenuItem.Text = "Hakkında";
+            this.hakkındaToolStripMenuItem.Click += new System.EventHandler(this.hakkındaToolStripMenuItem_Click);
+            // 
+            // programınAmacıToolStripMenuItem
+            // 
+            this.programınAmacıToolStripMenuItem.Name = "programınAmacıToolStripMenuItem";
+            this.programınAmacıToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.programınAmacıToolStripMenuItem.Text = "Programın Amacı";
+            this.programınAmacıToolStripMenuItem.Click += new System.EventHandler(this.programınAmacıToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(0, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(973, 39);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Durdur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button4.Location = new System.Drawing.Point(0, 292);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(973, 39);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Başlat";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(0, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(973, 39);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Liste Dosyasını Sil [Programlar.txt]";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(859, 230);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Başlangıçta Çalıştır";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // manuelEkleToolStripMenuItem
+            // 
+            this.manuelEkleToolStripMenuItem.Name = "manuelEkleToolStripMenuItem";
+            this.manuelEkleToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.manuelEkleToolStripMenuItem.Text = "Manuel Ekle";
+            this.manuelEkleToolStripMenuItem.Click += new System.EventHandler(this.manuelEkleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 409);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(989, 448);
+            this.MinimumSize = new System.Drawing.Size(989, 448);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Devam V2.0 By 20071999";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kaldırToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem hakkındaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programınAmacıToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripMenuItem manuelEkleToolStripMenuItem;
+    }
+}
+
---
+++ Devam/Form1.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Oku();
+            string keyName = @"HKEY_CURRENT_USer\Software\Microsoft\Windows\CurrentVersion\Run";
+            string valueName = "Devam V2.0";
+            if (Registry.GetValue(keyName, valueName, null) == null)
+            {
+                checkBox1.Checked = false;
+            }
+            else
+            {
+                checkBox1.Checked = true;
+            }
+        }
+        //Gerekli WinAPI'leri Importlıyoruz.
+        [DllImport("user32.dll")]
+        static extern int GetForegroundWindow();
+
+        [DllImport("user32")]
+        private static extern UInt32 GetWindowThreadProcessId(Int32 hWnd, out Int32 lpdwProcessId);
+       
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Int32 hwnd = 0;
+                hwnd = GetForegroundWindow();
+                string İslemAdi = Process.GetProcessById(GetWindowProcessID(hwnd)).ProcessName; //Önplanda olan işlemimizin işlem adını çekiyoruz.
+                label1.Text = İslemAdi;
+            }
+            catch (Exception ) {}
+        
+        }
+        private Int32 GetWindowProcessID(Int32 hwnd)
+        {
+            Int32 pid = 1;
+            GetWindowThreadProcessId(hwnd, out pid); 
+            return pid;
+        }
+
+        public void Oku()
+        {
+            try
+            {
+                if(File.Exists(Environment.CurrentDirectory+ "\\Programlar.txt"))
+                {
+                var satirlar = File.ReadAllLines(Environment.CurrentDirectory+ "\\Programlar.txt");
+                foreach (string satir in satirlar)
+                {
+                    string[] yazilar = satir.Split('|');
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = yazilar[0].ToString();
+                    lvi.SubItems.Add(yazilar[1].ToString());
+                    lvi.SubItems.Add(yazilar[2].ToString());
+                    listView2.Items.Add(lvi);
+                }
+                }
+            }
+            catch (Exception) { }
+        }
+        private void Kaydet(ListView lv, string splitter)
+        {
+            using (StreamWriter sw = new StreamWriter(Environment.CurrentDirectory+ "\\Programlar.txt"))
+            {
+                foreach (ListViewItem item in lv.Items)
+                {
+                    try
+                    {
+                        sw.WriteLine("{0}{1}{2}{3}{4}", item.Text, splitter, item.SubItems[1].Text, splitter, item.SubItems[2].Text);
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                }
+            }
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Int32 hwnd = 0;
+                hwnd = GetForegroundWindow();
+                string İslemAdi = Process.GetProcessById(GetWindowProcessID(hwnd)).ProcessName; //Önplanda olan işlem id'lerimizle istediklerimizi yapıyoruz.
+                string ProgramYolu = Process.GetProcessById(GetWindowProcessID(hwnd)).MainModule.FileName;
+                string ExecutableAdi = ProgramYolu.Substring(ProgramYolu.LastIndexOf(@"\") + 1);
+              
+                ListViewItem lvi = new ListViewItem(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(İslemAdi)); //Baş harflerin büyük olması için CulturInfo class'ımızı kullandık.
+                lvi.SubItems.Add(ProgramYolu);
+                lvi.SubItems.Add(ExecutableAdi);
+                ListViewItem item = listView1.FindItemWithText(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(İslemAdi));
+                if (item == null)
+                {
+                    listView1.Items.Add(lvi);
+                }
+                        if (lvi.Text == "Explorer")
+                        {
+                            listView1.Items.Remove(lvi);
+                        }
+                        
+                       
+                
+            }
+            catch (Exception) { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kaydet(listView1, "|");
+        }
+
+        private void kaldırToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem itemler in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(itemler);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView2.Items.Count >= 1)
+                {
+                    listView2.Items[0].Selected = true;
+                    listView2.Select();
+                    Process pc = new Process();
+                    pc.StartInfo.FileName = listView2.SelectedItems[0].SubItems[1].Text;
+                    pc.Start();
+                    listView2.Items.Remove(listView2.Items[0]);
+                }
+
+            }
+            catch (Exception ) {  }
+        }
+        /*
+         *  try
+            {
+                File.Delete(Environment.CurrentDirectory + @"\Programlar.txt");
+            }
+            catch (Exception) { }
+         */
+        
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yapımcı: 20071999\nKodlama Dili: C#\nTurkhackteam.org","Hakkında",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void programınAmacıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PC'yi açınca açıkken kullandığını programlar oto. açılır böylelikle tek tek çalıştığınız programları açmanıza gerek kalmaz.", "Programın Amacı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete(Environment.CurrentDirectory + @"\Programlar.txt");
+            }
+            catch (Exception) { }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            button4.Enabled = false;
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+            ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (checkBox1.Checked)
+                rk.SetValue("Devam V2.0", Application.ExecutablePath);
+            else
+                rk.DeleteValue("Devam V2.0", false);            
+        }
+
+        private void manuelEkleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Kısayol - Executable | *.lnk; *.exe";
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                ListViewItem lvi = new ListViewItem(of.FileName.Substring(of.FileName.LastIndexOf(@"\") + 1).Replace(".lnk",string.Empty).Replace(".exe",string.Empty));
+                lvi.SubItems.Add(of.FileName);
+                lvi.SubItems.Add(of.FileName.Substring(of.FileName.LastIndexOf(@"\") + 1));
+                listView1.Items.Add(lvi);
+            }
+        }
+
+      
+    }
+}
